db3846be603ffae5a9129974e9382ca4a1cc1ca1
Roby7xz <sostar.robert@gmail.com>
2022-07-27 14:46:27 +0200

    Added option play selected video

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-function VideoList({ videos }) {
+function VideoList({ videos, onVideoSelect }) {
 
 
     const renderedList = videos.map((video) => {
-        return <VideoItem key={video.id.videoId} video={video} />
+        return <VideoItem onVideoSelect={onVideoSelect} key={video.id.videoId} video={video} />
     });
 
 
